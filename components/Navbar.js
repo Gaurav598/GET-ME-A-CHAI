c48b07fa7e39126bcbe2f73e63139015e4b832d2
GAURAV KUMAR THAKUR <gr598895@gmail.com>
2025-10-15 12:31:07 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,17 +12,9 @@ const Navbar = () => {
     <nav className='bg-gray-900 shadow-xl shadow-white text-white flex justify-between items-center px-4 md:h-16'>
 
       <Link className="logo font-bold text-lg flex justify-center items-center" href={"/"}>
-        <img className='invertImg' src="tea.gif" width={44} alt="" />
+        <img className='invertImg' src="/tea.gif" width={44} alt="" />
         <span className='text-xl md:text-base my-3 md:my-0'>Get Me a Chai!</span>
       </Link>
-
-      {/* <ul className='flex justify-between gap-4'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Sign Up</li>
-        <li>Login</li>
-      </ul> */}
 
       <div className='relative flex justify-center items-center  md:block gap-4'>
         {session && <>
@@ -30,7 +22,14 @@ const Navbar = () => {
             setTimeout(() => {
               setShowdropdown(false)
             }, 100);
-          }} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white mx-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Account<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          }} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white mx-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+            
+            {/* ===== YAHAN CHANGE KIYA GAYA HAI ===== */}
+            <img src={session.user.image} alt="User Avatar" width={24} height={24} className="rounded-full mr-2" />
+            Account
+            {/* ======================================= */}
+
+            <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
             </svg>
           </button>
